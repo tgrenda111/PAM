c5b8824948ecdea362ecee667f6a5f840d386eed
tgrenda111 <grenda.tomek@gmail.com>
2025-11-16 15:28:06 +0100

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ScrollView, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, View, Text } from "react-native";
 
 import DateDisplay from "./src/tasks/task1/DateDisplay";
 import MapImage from "./src/tasks/task2/MapImage";
@@ -8,11 +8,21 @@ import FlexLayout from "./src/tasks/task4/FlexLayout";
 import UserName from "./src/tasks/task5/UserName";
 import UserSurname from "./src/tasks/task5/UserSurname";
 import LayoutWithBlocks from "./src/tasks/task6/LayoutWithBlocks";
+import InvitationCard from "./src/tasks/task7/InvitationCard";
+import DataTimer from "./src/tasks/task8/DataTimer";
+import Clock from "./src/tasks/task9/Clock";
+import RandomNumberGenerator from "./src/tasks/task10/RandomNumberGenerator";
 
 export default function App() {
+
+  const initialDateTime = new Date().toLocaleString();
+
+  const [randomNumber, setRandomNumber] = useState(0);
+
   return (
     <ScrollView>
       <View style={{ padding: 20 }}>
+        {/* Zadania 1–6 */}
         <DateDisplay />
         <MapImage />
         <StyledPizzaText />
@@ -20,6 +30,26 @@ export default function App() {
         <UserName name="Jan" />
         <UserSurname surname="Kowalski" />
         <LayoutWithBlocks />
+
+        {/* Zadanie 7 */}
+        <InvitationCard 
+          date="20.11.2025"
+          time="18:00"
+          location="Warszawa"
+          dressCode="Elegancki"
+        />
+
+        {/* Zadanie 8 */}
+        <DataTimer currentTime={initialDateTime} />
+
+        {/* Zadanie 9 */}
+        <Clock />
+
+        {/* Zadanie 10 */}
+        <RandomNumberGenerator onGenerate={setRandomNumber} />
+        <Text style={{ fontSize: 20, marginVertical: 10 }}>
+          Wylosowana liczba: {randomNumber}
+        </Text>
       </View>
     </ScrollView>
   );
